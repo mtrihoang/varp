7ef80056d389efed177bb8646b7684b33c268406
mtrihoang <trihoang30ec@gmail.com>
2025-09-23 22:48:57 +0200

    Update unit tests

--- tests/test_varp.py
+++ tests/test_varp.py
@@ -15,9 +15,19 @@ seed = 123456
 df = gen_data(n_obs, n_vars, mean, sd, A, seed).to_numpy()
 
 
-def test_irf():
+def test_symmetric_matrix():
     """
     Test the symmetry of the covariance matrix of residuals.
     """
-    output = estimate_reduced_form_VAR(df, p)[1]
-    assert np.allclose(output, output.T)
+    Sigma_U_hat = estimate_reduced_form_VAR(df, p)[1]
+    assert np.allclose(Sigma_U_hat, Sigma_U_hat.T), "The matrix must be symmetric"
+
+
+def test_positive_definite_matrix():
+    """
+    Test the symmetry of the covariance matrix of residuals.
+    """
+    Sigma_U_hat = estimate_reduced_form_VAR(df, p)[1]
+    assert (
+        np.linalg.eigvals(Sigma_U_hat) > 0
+    ).all(), "Matrix must be positive definite"
