e535ce80569b06ff1578dcfcef0b4453fd1d3996
mtrihoang <trihoang30ec@gmail.com>
2025-09-24 23:17:40 +0200

    Update IRF test

--- tests/test_varp.py
+++ tests/test_varp.py
@@ -2,6 +2,11 @@ import numpy as np
 import pytest
 from tests.gen_data import gen_data
 from varp.estimate_var import estimate_reduced_form_VAR
+from varp.irf import irf_coefs, irf_plots
+from varp.predict import pred_vars
+
+seed = 123456
+np.random.seed(seed)
 
 n_obs = 1000
 n_vars = 5
@@ -10,7 +15,9 @@ sd = 1
 p = 2
 h = 50
 A = np.random.uniform(-0.5, 0.5, size=(n_vars, n_vars))
-seed = 123456
+expected_irf_coefs = np.array(
+    [-0.17638068, -0.09145083, 0.30423662, -0.40200738, 0.07473255]
+)
 
 df = gen_data(n_obs, n_vars, mean, sd, A, seed)
 
@@ -31,3 +38,12 @@ def test_positive_definite_matrix():
     assert (
         np.linalg.eigvals(Sigma_U_hat) > 0
     ).all(), "Matrix must be positive definite"
+
+
+def test_irf():
+    """
+    Check the IRF calculation.
+    """
+    estimated_irf_coefs = irf_coefs(df, p, h)[1][1]
+    irf_diff = np.abs(estimated_irf_coefs - expected_irf_coefs)
+    assert np.all(irf_diff < 1e-6), "IRF calculation test failed!"
